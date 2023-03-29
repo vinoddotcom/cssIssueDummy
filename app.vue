cc1b7426ce05537dd856d9fde09556bec5d8eb01
vinoddotcom <vinod.yadav@vidyamantra.com>
2023-03-29 17:09:17 +0530

    Initail

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="h-screen w-full flex justify-center items-center bg-gradient-to-r from-purple-500 to-pink-500">
+    <p class="text-2xl text-white text-bold">hello, world!</p>
+  </div>
+</template>
